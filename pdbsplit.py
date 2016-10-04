fa4bdf045343baa6af6190b4fe5b205310c38bb1
Kort <Eric.Kort@vai.org>
2016-10-04 14:19:53 -0400

    initial commit

---
+++ pdbsplit.py
@@ -0,0 +1,18 @@
+from jlab import jlpdb
+
+if __name__ == "__main__":
+    """ Parses PDB files desired chain, and creates new PDB structures,
+        stripping all non-protein residues (inculding solvents and ligands)
+    """
+    import sys
+
+    if not len(sys.argv) >= 3:
+        print "Usage: $ python %s source chain [outdir]" % __file__
+        sys.exit()
+
+    if len(sys.argv) == 4:
+        splitter = jlpdb.Splitter(sys.argv[3])
+    else:
+        splitter = jlpdb.Splitter()
+
+    splitter.make_pdb(sys.argv[1], sys.argv[2])
